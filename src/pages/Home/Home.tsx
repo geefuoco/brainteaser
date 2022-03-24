7e242240eab8a42040e2f7e340ed63c530eaa8da
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-03-24 19:38:38 -0400

    Updated styles for home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,10 +4,10 @@ import "./Home.css";
 const Home: React.FC = () => {
   return (
     <main className="home">
-      <h1>
+      <h1 className="home-title">
         We believe everyone should have easy access to train their brains daily
       </h1>
-      <p>
+      <p className="home-description">
         Brainteaser offers simple, well designed mind games for you to train
         your brain every day
       </p>
